d3fa56c0557af949d2dc084ade514a32554befaf
nationaltransportindia <nationaltransportindia@gmail.com>
2026-07-21 13:58:15 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://nationaltransportindia.github.io/sales/";
-const SUPABASE_ANON_KEY = "sb_publishable_NhIlHd0F2tpiMrx6Wa9bbA_zRG_dS1S";
+const SUPABASE_URL = "https://uaodxnnxpjxptsgnnhyo.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_NhIlHd0F2tpiMrx6Wa9bbA_zRG_dS1S"; 
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
